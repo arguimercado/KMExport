401d85f03288e6ea1dbcac9a1702b51c17108638
amercado <arnold.mercado@hotmail.com>
2024-01-16 12:48:01 +0300

    Add Auto Dependency

---
+++ Core/src/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Core.src.Commons;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OfficeOpenXml;
+
+namespace Core.src;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddKMExportService(this IServiceCollection services,IConfiguration configuration)
+    {
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        services.AddScoped<IGeneralExport, GeneralExport>();
+        services.AddScoped<IExportObjectManager, ExportObjectManager>();
+
+        return services;
+    }
+}
